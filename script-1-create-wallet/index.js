8b8334920679ef8ddba7186bd7438ac9581d41bb
Bot80926 <mihail985biryukov1997@mail.ru>
2023-04-12 18:21:51 +0800

    docs: ✏️ update remark to english

--- script-1-create-wallet/index.js
+++ script-1-create-wallet/index.js
@@ -1,6 +1,6 @@
 /*
  * @LastEditors: Bot80926
- * @Description: 批量创建钱包
+ * @Description: Create wallet in batches
  * @FilePath: /ethers-scripts/script-1-create-wallet/index.js
  * Copyright (c) 2023 by Bot80926, All Rights Reserved.
  * Buy me a coffee: 0xa1ebF7E97Cfd6939fb90b27567AEBa5904a66630
@@ -13,7 +13,7 @@ const chalk = require("chalk");
 const createAccount = () => {
   let list = "";
 
-  //step1: 创建新账号
+  //step1: create wallet
   for (let i = 0; i < 100; i++) {
     const wallet = ethers.Wallet.createRandom();
 
@@ -24,12 +24,12 @@ const createAccount = () => {
     list += '\r "address: ' + address + '", privateKey: "' + pv + '", \r';
   }
 
-  //step2: 数据存档
+  //step2: create txt file and save wallet
   fs.writeFile(`./addr_key_book.txt`, list, (error) => {
     if (error) {
-      return console.log("批量创建钱包失败", error);
+      return console.log("create wallet failed, error: ", error);
     }
-    console.log(chalk.green("批量创建钱包成功 addr_key_book.txt 成功"));
+    console.log(chalk.green("Successfully created wallets in batches, file name: addr_key_book.txt"));
   });
 };
 
